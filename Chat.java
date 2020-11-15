40bc1cc25a3c78bc98dd7cca5b570f59e68b46bc
Imane <imanenajji@MacBook-Pro-SB.local>
2020-11-15 21:37:58 +0100

    Ajout fonctionnalité faireDuBruit pour les classes Chat et Chien

--- Chat.java
+++ Chat.java
@@ -1,9 +1,13 @@
 
-public class Chat extends Animal{
+public class Chat extends Animal implements Bruit{
 	
 	public void seDeplacer(int x, int y) {
 		super.seDeplacer(x, y);
 		System.out.println("Je bouge, Miaou!!!");
 	}
-
+	
+	public void faireDuBruit() {
+		System.out.println("Miaou!!!");
+	}
 }
+ 
--- Chien.java
+++ Chien.java
@@ -1,10 +1,13 @@
 
-public class Chien extends Animal{
+public class Chien extends Animal implements Bruit {
 	
 	public void seDeplacer(int x, int y) {
 		//TODO Auto-generated method stub
 		super.seDeplacer(x,y);
 		System.out.println("Je bouge, Wouf!!!");
 	}
-
+	
+	public void faireDuBruit() {
+		System.out.println("Wouf!!!"));
+	}
 }
